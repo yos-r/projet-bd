996ba197e34b1c29a163004e3fd6395b23176006
Yosr Barghouthi <84140616+yos-r@users.noreply.github.com>
2024-04-06 14:26:17 +0100

    First commit

--- src/main/java/com/isi/edt/Edt.java
+++ src/main/java/com/isi/edt/Edt.java
@@ -12,6 +12,6 @@ package com.isi.edt;
 public class Edt {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello Class!");
     }
 }
